18fc82a9ebac53e957abc6f3469804a155fa8ca9
Alexis K Grisham <alexisgrisham@AlexisGMac.local>
2020-06-07 14:03:15 -0400

    updatedteamlinkedins

--- src/components/home/Project.jsx
+++ src/components/home/Project.jsx
@@ -1,211 +1,274 @@
 import React from "react";
 
-
 const Project = () => {
-
-
   return (
-    <div id="projects" className="jumbotron jumbotron-fluid bg-transparent m-0">
- <h1 className="display-4 mb-5 text-center">Team</h1>
-<div className="row">
-<div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title">Cal Tiger </h5>
-          <p className="card-text"> Lead Organizer </p>
-
+      <div id="projects" className="jumbotron jumbotron-fluid bg-transparent m-0" >
+        <h1 className="display-4 mb-5 text-center">Team</h1>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+              <div className="card-body">
+                <h5 className="card-title">Cal Tiger </h5>
+                <p className="card-text"> Lead Organizer </p>
+                <p>
+                  <a href="https://www.linkedin.com/in/tigerventures/">
+                    LinkedIn
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+              <div className="card-body">
+                <h5 className="card-title"> Carol Bolger </h5>
+                <p className="card-text"> Advisor & Co-founder </p>
+                <p>
+                  <a href="https://www.linkedin.com/in/carolcorybolger/">
+                    LinkedIn
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+        <div className="col-md-6">
+          <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="card-body">
+              <h5 className="card-title">Dr. Dan Conway, PhD</h5>
+              <p className="card-text"> Associate Director </p>
+              <p>
+                <a href="https://www.linkedin.com/in/daniel-conway-b0a10b11/">
+                  LinkedIn
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="card-body">
+              <h5 className="card-title"> Christine Laferrierre, RN </h5>
+              <p className="card-text"> Director of Marketing </p>
+              <p>
+                <a href="https://www.linkedin.com/in/christine-laferriere-aaa49088/">
+                  LinkedIn
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
- </div>
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title">Dr. Dan Conway, PhD</h5>
-          <p className="card-text"> Associate Director </p>
+            <div className="row">
+        <div className="col-md-6">
+          <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="card-body">
+              <h5 className="card-title">Sandeep Seth</h5>
+              <p className="card-text"> CFO and Investor </p>
+              <p><a href="https://www.linkedin.com/in/sandeepsethcbsmba2011/">LinkedIn</a></p>
+            </div>
+          </div>
         </div>
-      </div>
- </div>
- </div>
- <div className="row">
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Christine Laferrierre, RN </h5>
-          <p className="card-text"> Director of Marketing  </p>
+        <div className="col-md-6">
+          <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="card-body">
+              <h5 className="card-title"> Charles Enslin </h5>
+              <p className="card-text"> Practice Director </p>
+              <p>
+                <a href="https://www.linkedin.com/in/charles-enslin-ab90821/">
+                  LinkedIn
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
- </div>
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title">Sandeep Seth</h5>
-          <p className="card-text"> CFO and Investor </p>
         </div>
-      </div>
- </div>
- </div>
- <div className="row">
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Charles Enslin </h5>
-          <p className="card-text"> Practice Director </p>
+        <div className="row">
+        <div className="col-md-6">
+          <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="card-body">
+              <h5 className="card-title"> Jeff Pewarski </h5>
+              <p className="card-text"> Director of Business Development </p>
+              <p>
+                <a href="https://www.linkedin.com/in/jeff-pewarski-mba-71879679/">
+                  LinkedIn
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
- </div>
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Jeff Pewarski </h5>
-          <p className="card-text"> Director of Business Development </p>
+        <div className="col-md-6">
+          <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="card-body">
+              <h5 className="card-title"> Rein Gottschalk </h5>
+              <p className="card-text"> Clearwater/Tallinn/Estonia </p>
+              <p>
+                <a href="https://www.linkedin.com/in/rein-t-gottschalk/">
+                  LinkedIn
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
- </div>
- </div> <div className="row">
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Rein Gottschalk </h5>
-          <p className="card-text"> Clearwater/Tallinn/Estonia </p>
         </div>
-      </div>
- </div>
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> FrankSev </h5>
-          <p className="card-text"> Board Member </p>
+        <div className="row">
+        <div className="col-md-6">
+          <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="card-body">
+              <h5 className="card-title"> Frank Severino </h5>
+              <p className="card-text"> Board Member </p>
+              <p>
+                <a href="http://veteranshealthnetwork.org/our-mission.php#">
+                  LinkedIn
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
- </div>
- </div> <div className="row">
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Dheeraj Krishna </h5>
-          <p className="card-text"> Student </p>
+        <div className="col-md-6">
+          <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="card-body">
+              <h5 className="card-title"> Dheeraj Krishna </h5>
+              <p className="card-text"> Student </p>
+              <p>
+                <a href="https://www.linkedin.com/in/dheeraj-yerabati-5b0568136/">
+                  LinkedIn
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
- </div>
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Krittin Kalra </h5>
-          <p className="card-text"> TheGlobalhack.com Prize winner, New Delhi </p>
         </div>
-      </div>
- </div>
- </div>
- <div className="row">
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Rehan Raj </h5>
-          <p className="card-text"> Hyderabad Young Entrepreneur </p>
+        <div className="row">
+        <div className="col-md-6">
+          <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="card-body">
+              <h5 className="card-title"> Krittin Kalra </h5>
+              <p className="card-text"> TheGlobalhack.com Prize winner, New Delhi
+              </p>
+              <p><a href="https://www.linkedin.com/in/krittinkalra/">LinkedIn</a></p>
+            </div>
+          </div>
         </div>
-      </div>
- </div>
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Dr. Frank Quintana, Phd </h5>
-          <p className="card-text"> Chief Data Scientist </p>
+        <div className="col-md-6">
+          <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="card-body">
+              <h5 className="card-title"> Rehan Raj </h5>
+              <p className="card-text"> Hyderabad Young Entrepreneur </p>
+              <p>
+                <a href="https://theglobalhack.com/news/the-10-year-old-hacking-coronavirus-instead-of-playing-fortnite/">
+                 Interview
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
- </div>
- </div>
-  <div className="row">
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Alexis Grisham </h5>
-          <p className="card-text"> Website Designer </p>
         </div>
-      </div>
- </div>
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Carol Bolger </h5>
-          <p className="card-text"> Software Developer </p>
+                <div className="row">
+        <div className="col-md-6">
+          <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="card-body">
+              <h5 className="card-title"> Dr. Frank Quintana, Phd </h5>
+              <p className="card-text"> Chief Data Scientist </p>
+              <p><a href="https://www.linkedin.com/in/frankquintanadataarchitect/">LinkedIn</a></p>
+            </div>
+          </div>
         </div>
-      </div>
- </div>
-</div>
-
-<h1 className="display-4 mb-5 text-center">Advisors</h1>
+        <div className="col-md-6">
+          <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+            <div className="card-body">
+              <h5 className="card-title"> Alexis Grisham </h5>
+              <p className="card-text"> Website Designer </p>
+              <p>
+                <a href="https://www.linkedin.com/in/alexiskjgrisham/">
+                  LinkedIn
+                </a>
+              </p>
+            </div>
+        </div>
+        </div>
+        </div>
 <div>
-   <div className="row">
-<div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Brian Kornfeld </h5>
-          <p className="card-text"> CEO, Synapse </p>
-          <p className="card-text"> Advisor </p>
+        <h1 className="display-4 mb-5 text-center">Advisors</h1>
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+                <div className="card-body">
+                  <h5 className="card-title"> Brian Kornfeld </h5>
+                  <p className="card-text"> CEO, Synapse </p>
+                  <p className="card-text"> Advisor </p>
+                  <p>
+                    <a href="https://www.linkedin.com/in/brian-kornfeld-mba-53613030/">
+                      LinkedIn
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+                <div className="card-body">
+                  <h5 className="card-title"> Aruna Sundararajan </h5>
+                  <p className="card-text"> Government of India </p>
+                  <p className="card-text"> Advisor </p>
+                  <p><a href="https://www.weforum.org/people/aruna-sundararajan">LinkedIn</a></p>
+                </div>
+              </div>
+            </div>
+            </div>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+                <div className="card-body">
+                  <h5 className="card-title"> Dan Conway, Phd </h5>
+                  <p className="card-text">
+                    Sam's Club Blockchain COE, Arkansas
+                  </p>
+                  <p className="card-text"> Advisor </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+                <div className="card-body">
+                  <h5 className="card-title"> Simi Ranajee </h5>
+                  <p className="card-text"> Healthcare SME </p>
+                  <p className="card-text"> Advisor and Second Keynote </p>
+                  <p><a href="https://www.linkedin.com/in/simi-ranajee-phd-mba-7a42503/"></a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+                <div className="card-body">
+                  <h5 className="card-title"> Vint Cerf </h5>
+                  <p className="card-text"> VP R&D </p>
+                  <p className="card-text"> Advisor </p>
+                  <p>
+                    <a href="https://www.linkedin.com/in/brian-kornfeld-mba-53613030/">
+                      LinkedIn
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card shadow-lg p-3 mb-5 bg-white rounded">
+                <div className="card-body">
+                  <h5 className="card-title"> Karen Vaugn-Kerns </h5>
+                  <p className="card-text"> MSN, APRN, CNM, FNP-C </p>
+                  <p className="card-text"> Advisor </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
- </div>
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Simi Ranajee </h5>
-          <p className="card-text"> Healthcare SME </p>
-          <p className="card-text"> Advisor and Second Keynote </p>
         </div>
-      </div>
- </div>
- </div>
- <div className="row">
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Dan Conway, Phd </h5>
-          <p className="card-text"> Sam's Club Blockchain COE, Arkansas </p>
-          <p className="card-text"> Advisor </p>
         </div>
-      </div>
- </div>
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Aruna Sundararajan </h5>
-          <p className="card-text"> Government of India </p>
-          <p className="card-text"> Advisor </p>
-        </div>
-      </div>
- </div>
- </div>
- <div className="row">
-  <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Vint Cerf </h5>
-          <p className="card-text"> VP R&D </p>
-          <p className="card-text"> Advisor </p>
-        </div>
-      </div>
- </div>
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Karen Vaugn-Kerns </h5>
-          <p className="card-text"> MSN, APRN, CNM, FNP-C </p>
-          <p className="card-text"> Advisor </p>
-        </div>
-      </div>
- </div>
- </div>
- <div className="col-md-6">
-      <div className="card shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="card-body">
-          <h5 className="card-title"> Sandeep Seth </h5>
-          <p className="card-text"> Local Entrepreneur </p>
-          <p className="card-text"> Advisor </p>
-        </div>
-      </div>
- </div>
-</div>
-    </div>
   );
 };
 
